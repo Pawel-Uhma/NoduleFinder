c69c457c7743c68c3fd11da0afe7ba2e683c4d93
pawel uhma <paweluhma136@gmail.com>
2025-05-12 08:10:06 +0200

    evaluate fix

--- src/evaluate.py
+++ src/evaluate.py
@@ -193,6 +193,9 @@ def evaluate_model(
     detections   = get_all_detections(model, dataloader, device, iou_threshold)
     prroc        = compute_pr_roc(detections)
     mAP_50_95, _ = compute_coco_map(model, dataloader, device, 0.5, 0.95, 0.05, confidence_threshold)
+
+    # TP/FP/FN/TN confusion matrix
+    TN_count = total - (TP_count + FP_count + FN_count)
     cm = np.array([[TN_count, FP_count],[FN_count, TP_count]])
     
     if verbose:
@@ -201,9 +204,7 @@ def evaluate_model(
         plot_f1_curve(prroc['f1'], prroc['pr_thresholds'], plots_dir)
         plot_roc_curve(prroc['fpr'], prroc['tpr'], prroc['roc_auc'], plots_dir)
 
-        # TP/FP/FN/TN confusion matrix
-        TN_count = total - (TP_count + FP_count + FN_count)
-        
+
         disp = ConfusionMatrixDisplay(confusion_matrix=cm,
                                       display_labels=['True Negative', 'True Positive'])
         fig, ax = plt.subplots()
